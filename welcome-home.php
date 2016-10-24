98b11e0dc4e03af08bdc746d4b72978ff1beb145
iYosuua Reyes <jesusrepi@gmail.com>
2016-10-24 14:35:27 +0200

    Modificado para que funcione el login con la BD.

--- welcome-home.php
+++ welcome-home.php
@@ -11,7 +11,7 @@ if(!isset($_SESSION['login_user']))
 <head>
 <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <meta charset="UTF-8">
-<title>PHP secure login script with encryption - click4knowledge.com</title>
+<title>Humiweb Private</title>
 <style>
 body {
     background-size: cover;
